beeffe30491026e791ba77a7822c2fb02f3e710b
romain-NLP <romain.gouron@gmail.com>
2017-04-13 15:59:15 -0300

    Add files via upload
    -Supprime les gros df au fur et à mesure dans crear_pares.py
    
    -Affiche matrice de confusion dans RF_alg.py
    
    -Inclut la Cross-validation (K=10), et la parallelisation des tâches (n_jobs=-1) dans  RF_alg.py

--- RF_alg.py
+++ RF_alg.py
@@ -2,8 +2,9 @@
 
 import pandas as pd
 import numpy as np
+from sklearn.metrics import confusion_matrix
+import itertools
 
-print "Training the random forest..."
 from sklearn import cross_validation
 from sklearn.ensemble import RandomForestClassifier
 
@@ -12,23 +13,73 @@ df_RF_train = pd.read_csv(path+'df_RF_train.csv',low_memory=False)
 df_q_vect_test = pd.read_csv(path+'df_q_vect_test.csv',low_memory=False)
 
 # Initialize a Random Forest classifier with 100 trees
-forest = RandomForestClassifier(n_estimators = 100) 
-
+forest = RandomForestClassifier(n_estimators = 100,n_jobs=-1,verbose=1) 
 print "RF inicializado"
 
-# Fit the forest to the training set, using the bag of words as 
-# features and the sentiment labels as the response variable
-#
-# This may take a few minutes to run
-forest = forest.fit(df_RF_train.ix[:,7:], df_RF_train["is_duplicate"])
-scores = cross_validation.cross_val_score(forest, df_RF_train.ix[:,7:], df_RF_train["is_duplicate"], cv=10)
+#Cross-validation
+kf = cross_validation.KFold(len(df_RF_train), n_folds=10)
+cm=np.zeros((2,2));
+for train_index, test_index in kf:
+
+   X_train, X_test = df_RF_train.iloc[train_index,7:], df_RF_train.iloc[test_index,7:]
+   y_train, y_test = df_RF_train.loc[train_index,'is_duplicate'], df_RF_train.loc[test_index,'is_duplicate']
+
+   forest.fit(X_train, y_train)
+   cm+=confusion_matrix(y_test, forest.predict(X_test))
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.savefig('title'+'.png')
+
+
+class_names=['not_dup','dup']
+plot_confusion_matrix(cm, classes=class_names,
+                      title='Confusion matrix, without normalization')
+plot_confusion_matrix(cm, classes=class_names, normalize=True,
+                      title='Normalized confusion matrix')
+
+
+#SUBMISSION / ENTREGA
+
+#  = forest.fit(df_RF_train.ix[:,7:], df_RF_train["is_duplicate"])
+# scores = cross_validation.cross_val_score(forest, df_RF_train.ix[:,7:], df_RF_train["is_duplicate"], cv=10,n_jobs=-1)
 # Take the mean of the scores (because we have one for each fold)
-print(scores.mean())
+# print(scores.mean())
 
-#predictions = forest.predict(df_q_vect_test.ix[:,4:])
-#submission = pandas.DataFrame({
+# predictions = forest.predict(df_q_vect_test.ix[:,4:])
+# submission = pandas.DataFrame({
 #        "is_duplicate": predictions,
- #       "test_id": df_q_vect_test["test_id"]
-  #  })
+#        "test_id": df_q_vect_test["test_id"]
+#    })
 
-#submission.to_csv(path+'sub_RF_1.csv',index=False)
+# submission.to_csv(path+'sub_RF_1.csv',index=False)
--- crear_pares.py
+++ crear_pares.py
@@ -1,8 +1,7 @@
 # -*-coding:Latin-1 -*
 import pandas as pd
 import numpy as np
-import re
-import nltk
+
 import herramientas as hr
 
 from gensim.models import word2vec
@@ -11,7 +10,7 @@ path = "/Users/Romain/Documents/UChile/Matematicas/Semestre_4/Memoria/Problemas/
 data_train = pd.read_csv(path+'train.csv',low_memory=False)#.sample(10000, random_state=23)
 data_test = pd.read_csv(path+'test.csv',low_memory=False)#.sample(100000, random_state=23)
 
-print "datos importados"
+print "Datos importados"
 
 
 data_train = hr.clean_dataframe(data_train)
@@ -37,12 +36,19 @@ corpus = hr.build_corpus(data_tot)
 model = word2vec.Word2Vec(corpus, size=100, window=20, min_count=200, workers=4)
 
 print "Model word2vec creado"
-            
+model.save(path+'mymodel')
 
-#Matriz que contiene los vectores promedio en cada fila
+#Train : Matriz que contiene los vectores promedio en cada fila
 corpus_mat_train = np.zeros((2*N,100));
 for i in range(2*N):
     corpus_mat_train[i,:]=hr.promedio(corpus[i],model)
+
+#Test : Matriz que contiene los vectores promedio en cada fila
+corpus_mat_test = np.zeros((2*M,100));
+for i in range(2*M):
+    corpus_mat_test[i,:]=hr.promedio(corpus[i+2*N],model)
+
+del corpus, model #para aliviar la RAM
 
 #Matriz (vec_Q1 | vec_Q2)
 corpus_mat_train=np.concatenate((corpus_mat_train[:N,:],corpus_mat_train[N:,:]),axis=1)
@@ -52,25 +58,27 @@ df_vect_train=pd.DataFrame(corpus_mat_train,index=data_train.index)
 corpus_mat_inv=np.concatenate((corpus_mat_train[:,100:],corpus_mat_train[:,:100]),axis=1)
 df_vect_inv_train=pd.DataFrame(corpus_mat_inv,index=data_train.index)
 
+#df que contienen las dos matrices anteriores
 df_q_vect=pd.concat([data_train,df_vect_train],axis=1)
 df_q_vect_inv=pd.concat([data_train,df_vect_inv_train],axis=1)
 
+del data_train, corpus_mat_train, corpus_mat_inv, df_vect_train, df_vect_inv_train
+
 df_RF_train=pd.concat([df_q_vect,df_q_vect_inv],axis=0)
 
-print "Conjunto de entrenamiento creado"
-#Matriz que contiene los vectores promedio en cada fila
-corpus_mat_test = np.zeros((2*M,100));
-for i in range(2*M):
-    corpus_mat_test[i,:]=hr.promedio(corpus[i+2*N],model)
+del df_q_vect, df_q_vect_inv
 
+print "Conjunto de entrenamiento creado"
+
+#Test 
 #Matriz (vec_Q1 | vec_Q2)
 corpus_mat_test=np.concatenate((corpus_mat_test[:M,:],corpus_mat_test[M:,:]),axis=1)
 df_vect_test=pd.DataFrame(corpus_mat_test,index=data_test.index)
 
 df_q_vect_test=pd.concat([data_test,df_vect_test],axis=1)
 print "Conjunto de test creado"
-del data_train, data_test, corpus_mat_train, corpus_mat_test, corpus_mat_inv, corpus
-del df_vect_train, df_vect_test, df_q_vect, df_q_vect_inv, df_vect_inv_train
+del data_test, corpus_mat_test
+del df_vect_test, df_q_vect, df_q_vect_inv
 
 df_RF_train.to_csv(path+'df_RF_train.csv',index=False)
 df_q_vect_test.to_csv(path+'df_q_vect_test.csv',index=False)
